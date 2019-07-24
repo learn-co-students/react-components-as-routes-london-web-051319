fc7d256c73dddf02bc56da21e526385eb302ef3c
James <hayward.jp@gmail.com>
2019-07-24 14:28:00 +0100

    Done.

---
+++ src/About.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+// export default class About extends Component {
+//   render() {
+//     return (
+//       <div>
+//         <h1>About!</h1>
+//       </div>
+//     )
+//   }
+// }
+
+const About = () => <h1>This is my about component!</h1>;
---
+++ src/separate_index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {BrowserRouter as Router, Route, NavLink} from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
+import Login from "./Login";
+import Navbar from "./Navbar";
+
+const link = {
+  width: '100px',
+  padding: '12px',
+  margin: '0 6px 6px',
+  background: 'blue',
+  textDecoration: 'none',
+  color: 'white',
+}
+
+ReactDOM.render((
+  <Router>
+    <React.Fragment>
+      <Route path="/" component={Home} />
+      {/* <Route exact path="/" component={Home} /> */}
+      <Route exact path="/about" component={About} />
+      <Route exact path="/login" component={Login} />
+    </React.Fragment>
+  </Router>
+),
+  document.getElementById('root')
+);
