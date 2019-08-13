61611783e35c6cdee6d59d003d11e6d719dc2a5d
Rudi Hinds <rudihinds@gmail.com>
2019-08-13 10:25:34 +0100

    Done.

---
+++ src/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
+
+
+
+export default function About () {
+    return (
+      <div>
+        <h1>My about component!</h1>
+      </div>
+    )
+  }
